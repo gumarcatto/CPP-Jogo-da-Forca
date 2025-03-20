1ebd9847c73b12a9c9b3fd8e07110c63ee734e52
Gustavo Marcatto <gu80l4@gmail.com>
2025-03-19 22:35:24 -0300

    separado duas funçoes "letra_existe" e "nao_acertou"

---
+++ nao_acertou.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <map>
+
+using namespace std;
+
+string palavra_secreta;
+map<char, bool> chutou;
+
+bool nao_acertou(){
+    for(char letra: palavra_secreta){
+        if(!chutou[letra]){
+            return true;
+        }
+    }
+    return false;
+}
